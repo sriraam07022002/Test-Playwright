200271478616eff1766a697a6cdd1c8a902721c8
Sriram <sriraam.jothibasu@codem.com>
2025-07-28 18:39:29 +0530

    Test Update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Sample.test', async ({ page }) => {
   await page.goto('https://www.google.com/');
   await expect(page).toHaveTitle(/Google/);
+  await page.locator('#APjFqb').fill('Playwright'); // Wait for suggestions to load
 });
